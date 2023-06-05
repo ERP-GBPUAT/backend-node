fe9eb5da59db50ecc0f9e7693da5135a5dbb2ea7
Udit <royalboy0012@gmail.com>
2023-06-06 01:33:54 +0530

    done

--- routes/nodues.ts
+++ routes/nodues.ts
@@ -1,12 +1,17 @@
 import express from "express";
-import { applyNoDues, approveNoDues, getAdviseeNoDues, getNoDues } from "../controllers/NoDues/nodues";
+import { applyNoDues, approveNoDues, getAdviseeNoDues, getAllNoDues, getNoDues } from "../controllers/NoDues/nodues";
 import authentication from "../middleware/authentication";
+import { applyEmpNoDues, approveEmpNoDues, getAllEmpNoDues } from "../controllers/NoDues/empNoDues";
 
 const router = express.Router();
 
+router.post("/applyEmpNodue",authentication,applyEmpNoDues)
 router.post("/applyNodue",authentication,applyNoDues)
 router.get("/getNodueAdvisor",authentication,getAdviseeNoDues)
+router.get("/getAllNddue",authentication,getAllNoDues)
 router.get("/getNodue",authentication,getNoDues)
 router.post("/approveNodue",authentication,approveNoDues)
+router.get("/getAllEmpNddue",authentication,getAllEmpNoDues)
+router.post("/approveEmpNodue",authentication,approveEmpNoDues)
 
 export default router
